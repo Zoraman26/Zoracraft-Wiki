26357f0393530d5544cdff2839f8b3f4acb3210f
Aza K9 <58900639+baphomutt@users.noreply.github.com>
2022-06-23 03:01:52 -0500

    Update scrapewikihtml.js

--- scripts/scrapewikihtml.js
+++ scripts/scrapewikihtml.js
@@ -1,1 +1,8 @@
+/*
 
+This needs to go through the tiddlers folder for all (or just recently changed ones -- detect in future)
+and traverse localhost via curl or other to scrape and save generated tiddlywiki html from each tiddler.
+
+Optimize and cleanup later; get it working now!
+
+*/
